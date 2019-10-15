812b463da6878371572aa9c8d83f8eeaff4eaf90
Peter Kraft <petereliaskraft@gmail.com>
2019-10-15 15:06:13 -0700

    gRPC scaffold code

--- src/main/java/edu/stanford/futuredata/uniserve/broker/Broker.java
+++ src/main/java/edu/stanford/futuredata/uniserve/broker/Broker.java
@@ -4,6 +4,10 @@ import edu.stanford.futuredata.uniserve.interfaces.QueryEngine;
 
 public class Broker {
 
+    private final QueryEngine queryEngine;
+
     public Broker(QueryEngine queryEngine) {
+        this.queryEngine = queryEngine;
     }
 }
+
--- src/main/java/edu/stanford/futuredata/uniserve/datastore/DataStore.java
+++ src/main/java/edu/stanford/futuredata/uniserve/datastore/DataStore.java
@@ -1,9 +1,31 @@
 package edu.stanford.futuredata.uniserve.datastore;
 
+import edu.stanford.futuredata.uniserve.QueryDataGrpc;
 import edu.stanford.futuredata.uniserve.interfaces.ShardFactory;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 
 public class DataStore {
 
-    public DataStore(ShardFactory shardFactory) {
+    private final Server server;
+
+    public DataStore(int port, ShardFactory shardFactory) {
+        ServerBuilder serverBuilder = ServerBuilder.forPort(port);
+        this.server = serverBuilder.addService(new QueryDataService(shardFactory))
+                .build();
+    }
+
+    public void startServing() {
+
+    }
+
+    private static class QueryDataService extends QueryDataGrpc.QueryDataImplBase {
+
+        private final ShardFactory shardFactory;
+
+        QueryDataService(ShardFactory shardFactory) {
+            this.shardFactory = shardFactory;
+        }
+
     }
 }
