8d5585dee9d1c13b538ab1380bf4ef2029c97c0e
Peter Kraft <petereliaskraft@gmail.com>
2020-02-13 10:56:01 -0800

    Revert "EXPERIMENTAL server-side read query response compression"
    This reverts commit 09554396a3e3eba5857743a2c956759b3e47f087.

--- src/main/java/edu/stanford/futuredata/uniserve/datastore/ServiceBrokerDataStore.java
+++ src/main/java/edu/stanford/futuredata/uniserve/datastore/ServiceBrokerDataStore.java
@@ -8,7 +8,6 @@ import edu.stanford.futuredata.uniserve.interfaces.Row;
 import edu.stanford.futuredata.uniserve.interfaces.Shard;
 import edu.stanford.futuredata.uniserve.interfaces.WriteQueryPlan;
 import edu.stanford.futuredata.uniserve.utilities.Utilities;
-import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
 import org.javatuples.Pair;
 import org.slf4j.Logger;
@@ -138,11 +137,8 @@ class ServiceBrokerDataStore<R extends Row, S extends Shard> extends BrokerDataS
 
     @Override
     public void readQuery(ReadQueryMessage request,  StreamObserver<ReadQueryResponse> responseObserver) {
-        ServerCallStreamObserver<ReadQueryResponse> callStreamObserver =
-                (ServerCallStreamObserver<ReadQueryResponse>) responseObserver;
-        callStreamObserver.setCompression("gzip");
-        callStreamObserver.onNext(readQueryHandler(request));
-        callStreamObserver.onCompleted();
+        responseObserver.onNext(readQueryHandler(request));
+        responseObserver.onCompleted();
     }
 
     private ReadQueryResponse readQueryHandler(ReadQueryMessage readQuery) {
