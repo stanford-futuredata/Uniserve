adbd0febc14cd3955c1178ddb750286712610c9c
Peter Kraft <petereliaskraft@gmail.com>
2020-05-04 17:47:43 -0500

    Update DataStore.java

--- src/main/java/edu/stanford/futuredata/uniserve/datastore/DataStore.java
+++ src/main/java/edu/stanford/futuredata/uniserve/datastore/DataStore.java
@@ -59,7 +59,7 @@ public class DataStore<R extends Row, S extends Shard> {
     private final UploadShardDaemon uploadShardDaemon;
     public static int uploadThreadSleepDurationMillis = 10000;
 
-    public static int qpsReportTimeInterval = 120; // In seconds.
+    public static int qpsReportTimeInterval = 60; // In seconds -- should be same as load balancer interval.
 
     public boolean runPingDaemon = true; // Public for testing
     private final PingDaemon pingDaemon;
