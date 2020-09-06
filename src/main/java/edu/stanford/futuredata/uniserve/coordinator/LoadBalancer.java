6f1671a1277b7159a6fd099dbe38f15d7d48bd33
Peter Kraft <petereliaskraft@gmail.com>
2020-09-06 10:43:10 -0500

    Replication

--- src/main/java/edu/stanford/futuredata/uniserve/coordinator/LoadBalancer.java
+++ src/main/java/edu/stanford/futuredata/uniserve/coordinator/LoadBalancer.java
@@ -34,33 +34,138 @@ public class LoadBalancer {
             int shardLoad = shardLoads.get(shardNum);
             List<Integer> serverNums = consistentHash.getBuckets(shardNum);
             for (Integer serverNum: serverNums) {
-                serverLoads.merge(serverNum, shardLoad, Integer::sum);
+                serverLoads.merge(serverNum, shardLoad / serverNums.size(), Integer::sum);
                 serverToShards.get(serverNum).add(shardNum);
             }
         }
         PriorityQueue<Integer> serverMinQueue = new PriorityQueue<>(Comparator.comparing(serverLoads::get));
-        PriorityQueue<Integer> serverMaxQueue = new PriorityQueue<>(Comparator.comparing(serverLoads::get).reversed());
         serverMinQueue.addAll(serverLoads.keySet());
+        PriorityQueue<Integer> serverMaxQueue = new PriorityQueue<>(Comparator.comparing(serverLoads::get).reversed());
         serverMaxQueue.addAll(serverLoads.keySet());
+
+        // Replication.
+        int averageServerLoad = shardLoads.values().stream().mapToInt(i -> i).sum() / consistentHash.buckets.size();
+        for (Map.Entry<Integer, Integer> shardLoad: shardLoads.entrySet()) {
+            Integer shardNum = shardLoad.getKey();
+            int load = shardLoad.getValue();
+            int targetReplicas = (load % averageServerLoad == 0) ? load / averageServerLoad :
+                    load / averageServerLoad + 1;
+            targetReplicas = (targetReplicas == 0) ? 1 : targetReplicas;
+            targetReplicas = Math.min(targetReplicas, serverLoads.size());
+            int numReplicas = consistentHash.reassignmentMap.containsKey(shardNum) ?
+                    consistentHash.reassignmentMap.get(shardNum).size() : 1;
+            if (numReplicas < targetReplicas) {
+                // Add replica
+                Integer targetServer = null;
+                while (targetServer == null && !serverMinQueue.isEmpty()) {
+                    Integer server = serverMinQueue.remove();
+                    if (!serverToShards.get(server).contains(shardNum) && !lostShards.contains(server)) {
+                        targetServer = server;
+                    }
+                }
+                if (targetServer == null) {
+                    continue;
+                }
+                logger.info("Add Replica of Shard {} on DS{}", shardNum, targetServer);
+                serverToShards.get(targetServer).add(shardNum);
+                List<Integer> currentlyAssigned = consistentHash.getBuckets(shardNum);
+                int originallyAssigned = currentlyAssigned.size();
+                if (consistentHash.reassignmentMap.containsKey(shardNum)) {
+                    consistentHash.reassignmentMap.get(shardNum).add(targetServer);
+                } else {
+                    consistentHash.reassignmentMap.put(shardNum,
+                            new ArrayList<>(List.of(targetServer, consistentHash.getRandomBucket(shardNum))));
+                }
+                Integer targetServerLoad = serverLoads.get(targetServer);
+                serverLoads.put(targetServer, targetServerLoad + load / (originallyAssigned + 1));
+                gainedShards.add(targetServer);
+                serverMaxQueue.remove(targetServer);
+                serverMaxQueue.add(targetServer);
+                serverMinQueue.add(targetServer);
+                for (Integer currentServer: currentlyAssigned) {
+                    Integer currentServerLoad = serverLoads.get(currentServer);
+                    int loadChange = load / (originallyAssigned + 1) - load / originallyAssigned;
+                    serverLoads.put(currentServer, currentServerLoad + loadChange);
+                    serverMaxQueue.remove(currentServer);
+                    serverMaxQueue.add(currentServer);
+                    serverMinQueue.remove(currentServer);
+                    serverMinQueue.add(currentServer);
+                }
+            } else if (numReplicas > targetReplicas) {
+                // Remove replica
+                Integer targetServer = null;
+                List<Integer> removedServers = new ArrayList<>();
+                while (targetServer == null && !serverMaxQueue.isEmpty()) {
+                    Integer server = serverMaxQueue.remove();
+                    if (serverToShards.get(server).contains(shardNum) && !gainedShards.contains(server)) {
+                        targetServer = server;
+                    } else {
+                        removedServers.add(server);
+                    }
+                }
+                if (targetServer == null) {
+                    continue;
+                }
+                logger.info("Remove Replica of Shard {} from DS{}", shardNum, targetServer);
+                serverMaxQueue.addAll(removedServers);
+                serverToShards.get(targetServer).remove(shardNum);
+                List<Integer> currentlyAssigned = consistentHash.getBuckets(shardNum);
+                int originallyAssigned = currentlyAssigned.size();
+                assert(originallyAssigned > 1);
+                assert(consistentHash.reassignmentMap.containsKey(shardNum));
+                consistentHash.reassignmentMap.get(shardNum).remove(targetServer);
+                assert(consistentHash.reassignmentMap.get(shardNum).size() > 0);
+                Integer targetServerLoad = serverLoads.get(targetServer);
+                serverLoads.put(targetServer, targetServerLoad - load / originallyAssigned);
+                lostShards.add(targetServer);
+                serverMinQueue.remove(targetServer);
+                serverMinQueue.add(targetServer);
+                serverMaxQueue.add(targetServer);
+                for (Integer currentServer: currentlyAssigned) {
+                    Integer currentServerLoad = serverLoads.get(currentServer);
+                    int loadChange =  load / (originallyAssigned - 1) - load / originallyAssigned;
+                    serverLoads.put(currentServer, currentServerLoad + loadChange);
+                    serverMaxQueue.remove(currentServer);
+                    serverMaxQueue.add(currentServer);
+                    serverMinQueue.remove(currentServer);
+                    serverMinQueue.add(currentServer);
+                }
+            }
+        }
+
+        // Balancing.
+        serverMinQueue.clear();
+        serverMinQueue.addAll(serverLoads.keySet().stream().filter(i -> !lostShards.contains(i)).collect(Collectors.toList()));
+        serverMaxQueue.clear();
+        serverMaxQueue.addAll(serverLoads.keySet().stream().filter(i -> !gainedShards.contains(i)).collect(Collectors.toList()));
+
         double averageLoad = shardLoads.values().stream().mapToDouble(i -> i).sum() / serverLoads.size();
         double epsilon = averageLoad / epsilonRatio;
         while (serverMaxQueue.size() > 0 && serverLoads.get(serverMaxQueue.peek()) > averageLoad + epsilon) {
             Integer overLoadedServer = serverMaxQueue.remove();
-            while (serverToShards.get(overLoadedServer).size() > 0 && serverLoads.get(overLoadedServer) > averageLoad + epsilon) {
+            while (serverToShards.get(overLoadedServer).stream().anyMatch(i -> shardLoads.get(i) > 0)
+                    && serverLoads.get(overLoadedServer) > averageLoad + epsilon) {
                 Integer underLoadedServer;
                 if (newServer == null || serverLoads.get(newServer) > averageLoad + epsilon) {
                     underLoadedServer = serverMinQueue.remove();
                 } else {
                     underLoadedServer = newServer;
                 }
-                Integer mostLoadedShard = serverToShards.get(overLoadedServer).stream().filter(i -> shardLoads.get(i) > 0).max(Comparator.comparing(shardLoads::get)).orElse(null);
+                Integer mostLoadedShard = serverToShards.get(overLoadedServer).stream().filter(i -> shardLoads.get(i) > 0)
+                        .max(Comparator.comparing(i -> shardLoads.get(i) / consistentHash.getBuckets(i).size())).orElse(null);
                 assert(mostLoadedShard != null);
+                int mostLoadedShardLoad = shardLoads.get(mostLoadedShard) / consistentHash.getBuckets(mostLoadedShard).size();
                 serverToShards.get(overLoadedServer).remove(mostLoadedShard);
-                if (serverLoads.get(underLoadedServer) + shardLoads.get(mostLoadedShard) <= averageLoad + epsilon &&
+                if (serverLoads.get(underLoadedServer) + mostLoadedShardLoad <= averageLoad + epsilon &&
                         (newServer == null || overLoadedServer.equals(newServer) || underLoadedServer.equals(newServer))) {
-                    consistentHash.reassignmentMap.put(mostLoadedShard, new ArrayList<>(List.of(underLoadedServer)));
-                    serverLoads.merge(overLoadedServer, -1 * shardLoads.get(mostLoadedShard), Integer::sum);
-                    serverLoads.merge(underLoadedServer, shardLoads.get(mostLoadedShard), Integer::sum);
+                    if (consistentHash.reassignmentMap.containsKey(mostLoadedShard)) {
+                        consistentHash.reassignmentMap.get(mostLoadedShard).remove(overLoadedServer);
+                        consistentHash.reassignmentMap.get(mostLoadedShard).add(underLoadedServer);
+                    } else {
+                        consistentHash.reassignmentMap.put(mostLoadedShard, new ArrayList<>(List.of(underLoadedServer)));
+                    }
+                    serverLoads.merge(overLoadedServer, -1 * mostLoadedShardLoad, Integer::sum);
+                    serverLoads.merge(underLoadedServer, mostLoadedShardLoad, Integer::sum);
                     lostShards.add(overLoadedServer);
                     gainedShards.add(underLoadedServer);
                     logger.info("Shard {} transferred from DS{} to DS{}", mostLoadedShard, overLoadedServer, underLoadedServer);
